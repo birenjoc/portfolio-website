aaff0a9449bbbd71659a57c52c3d9c501744b33d
birenjoc <118249213+birenjoc@users.noreply.github.com>
2022-12-21 19:00:56 +0545

    updated app.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
 
     <>
     <Routes>
+        <Route path='/' element={<Home/>}/>
         <Route path='Home/' element={<Home/>}/>
         <Route path='Project/' element={<Project/>}/>
         <Route path='About/' element={<About/>}/>

680af75d2d81fb0b31b33703cd04fe835ea55eb2
Birendra Joshi <bimaljoshi85@gmail.com>
2022-12-22 20:27:25 +0545

    new commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Heroimg from '../Components/Heroimg';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+
 const Home = () => {
   return (
   <div>
   <Navbar/>
   <Heroimg/>
+   
   <Footer/>
  </div>
  );
